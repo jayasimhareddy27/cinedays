6f2adb51e1298b4ea620a165e877a48f1f2d75d5
jayasimhareddy27 <jayasimhareddy27@gmail.com>
2022-10-24 19:01:51 +0530

    full route4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -32,11 +32,11 @@ const App=()=>{
                 </div>
                 <div className='bg-white dark:bg-black p-3 h-screen overflow-scroll pb-32'>
                     <Routes >
-                    <Route path='/'  exact element={<MovieList/>} ></Route>
+                        <Route path='cinedays'  exact element={<MovieList/>} ></Route>
                         <Route path='/cinedays/profile/:id' element={<Profile/>}></Route>
                         <Route path='/cinedays/Movie/:id' element={<Movieinformation/>}></Route>
                         <Route path='/cinedays/Actor/:id' element={<Actors/>}></Route>
-                        <Route path='*' exact element={<Navigate replace  to="/" />} ></Route>
+                        <Route path='*' exact element={<Navigate replace  to="cinedays" />} ></Route>
                     </Routes>
                 
                 </div>
